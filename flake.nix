a59cee080ee6d28432ecd884f2b3900bb2c71279
AdrienDML <adriendml99@protonmail.com>
2024-08-16 14:34:41 +0200

    Updated environements

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: flake-utils.lib.eachDefaultSystem (system: 
+  let 
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    lib.pkgs = pkgs;
+
+    devShells.default = pkgs.mkShell {
+        # All the programs i need to edit my config.
+        packages = with pkgs; [
+          # The nix lsp i use.
+          nil
+          # to run the nix flake commands.
+          just
+          nushell
+        ];
+    };
+  });
+}
